8e50435658d8ba48ce3cd055475f29e5144fb5dc
marianorapa <marianorapaport@gmail.com>
2021-02-28 20:07:54 -0300

    add CORS support

--- splitter/src/main/java/com/mrapaport/unlu/sdypp/splitter/controller/ImageReceiverController.java
+++ splitter/src/main/java/com/mrapaport/unlu/sdypp/splitter/controller/ImageReceiverController.java
@@ -3,14 +3,18 @@ package com.mrapaport.unlu.sdypp.splitter.controller;
 import com.mrapaport.unlu.sdypp.splitter.dto.ImagesReceivedResponseDTO;
 import com.mrapaport.unlu.sdypp.splitter.service.JWTProvider;
 import com.mrapaport.unlu.sdypp.splitter.service.ProcessorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +22,8 @@ import java.util.UUID;
 
 @RestController
 @PropertySource("classpath:application.properties")
-@CrossOrigin(origins = "${cors.allowed.origins}")
+//@CrossOrigin(origins = {"http://web-ui-service", "http://localhost:3000"})
+@CrossOrigin(origins = "*")
 public class ImageReceiverController {
 
     @Autowired
@@ -27,9 +32,13 @@ public class ImageReceiverController {
     @Autowired
     JWTProvider jwtProvider;
 
+    Logger logger = LoggerFactory.getLogger(ImageReceiverController.class);
+
     @PostMapping("/process-images")
-    public ResponseEntity<ImagesReceivedResponseDTO> receiveImages(@RequestParam List<MultipartFile> images)
+    public ResponseEntity<ImagesReceivedResponseDTO> receiveImages(@RequestPart("images") List<MultipartFile> images)
             throws IOException, ServletException {
+
+        logger.info("Process images request: {}", images);
 
         UUID uuid = processorService.processImages(images);
 
